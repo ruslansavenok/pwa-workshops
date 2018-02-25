348ab67bd4c915fb84beaba981e3fa5b902af436
Ruslan <ruslan.savenok@gmail.com>
2018-02-25 12:46:40 +0100

    fx

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LazyLoad from 'react-lazyload';
 
 const Picture = ({ src, ...props }) => (
-  <LazyLoad height="100%">
+  <LazyLoad height="100%" once>
     <picture>
       <source srcSet={src + '?format=webp'} type="image/webp" />
       <img src={src} {...props} />
