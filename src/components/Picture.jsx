2a3099f225abfa43205fb06bda22650ae816b6d5
Ruslan <ruslan.savenok@gmail.com>
2018-02-25 11:54:47 +0100

    Lazy load

--- src/components/Picture.jsx
+++ src/components/Picture.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import LazyLoad from 'react-lazyload';
 
 const Picture = ({ src, ...props }) => (
-  <picture>
-    <source srcSet={src + '?format=webp'} type="image/webp" />
-    <img src={src} {...props} />
-  </picture>
+  <LazyLoad height="100%">
+    <picture>
+      <source srcSet={src + '?format=webp'} type="image/webp" />
+      <img src={src} {...props} />
+    </picture>
+  </LazyLoad>
 );
 
 export default Picture;
